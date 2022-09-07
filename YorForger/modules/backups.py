382293241fa915bc142391eeffb2961aa8d3b2ed
Rachit Pal <rachit2380@gmail.com>
2022-09-07 16:48:25 +0530

    Update backups.py

--- YorForger/modules/backups.py
+++ YorForger/modules/backups.py
@@ -377,7 +377,7 @@ __mod_name__ = "Backup"
 __help__ = """
 *Only for group owner:*
 
- ✪︎ /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ ✪︎ /import: Reply to the backup file for the Yor group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
  ✪︎ /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
